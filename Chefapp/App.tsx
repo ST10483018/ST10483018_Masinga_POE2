b5fd50656fa5fe01630ceb4f6595f1624ed3bdf9
ST10483018 <ST10483018@imconnect.edu.za>
2025-11-10 15:14:13 +0200

    fixed App.tsx
    made adjustment to my app.tsx to fit requirement needed

--- Chefapp/App.tsx
+++ Chefapp/App.tsx
@@ -1,21 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./Screens/Homepage";
-import AddEditDish from "./Screens/AddEditDish";
-import { MenuProvider } from "./MenuContext";
+import { SafeAreaView, StatusBar } from "react-native";
 
-const Stack = createNativeStackNavigator();
+import Homepage from "./Screens/Homepage";
+import AddEditDish from "./Screens/AddEditDish";
+import FilterMenu from "./Screens/FilterMenu";
+
+export type Dish = {
+  id: string;
+  name: string;
+  description?: string;
+  price: number;
+  course: "starters" | "mains" | "desserts";
+};
+
+export type RootStackParamList = {
+  Landing: undefined;
+  Home: undefined;
+  AddEditDish: undefined;
+  FilterMenu: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [menu, setMenu] = useState<Dish[]>([
+    {
+      id: "1",
+      name: "Garlic Bread",
+      description: "Crispy bread with garlic butter.",
+      price: 40,
+      course: "starters",
+    },
+    {
+      id: "2",
+      name: "Steak and Chips",
+      description: "Juicy sirloin with fries.",
+      price: 120,
+      course: "mains",
+    },
+    {
+      id: "3",
+      name: "Chocolate Cake",
+      description: "Rich chocolate cake.",
+      price: 65,
+      course: "desserts",
+    },
+  ]);
+
   return (
-    <MenuProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Menu" component={Home} />
-          <Stack.Screen name="AddEditDish" component={AddEditDish} options={{ title: "Add / Edit Dish" }} />
+          <Stack.Screen
+            name="Home"
+            options={{ title: "Menu" }}
+          >
+            {(props) => <Homepage {...props} menu={menu} setMenu={setMenu} />}
+          </Stack.Screen>
+
+          <Stack.Screen
+            name="AddEditDish"
+            options={{ title: "Add / Edit Dish" }}
+          >
+            {(props) => <AddEditDish {...props} menu={menu} setMenu={setMenu} />}
+          </Stack.Screen>
+
+          <Stack.Screen
+            name="FilterMenu"
+            options={{ title: "Filter Menu" }}
+          >
+            {(props) => <FilterMenu {...props} menu={menu} />}
+          </Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
-    </MenuProvider>
+    </SafeAreaView>
   );
 }
